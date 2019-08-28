9b7e1cca6de8d7aad643d283520f703e8237e65c
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-08-28 09:32:24 +0300

    Add calculation functionalities to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 function add(number1, number2) {
     return number1 + number2;
     }
-    var number1 = parseInt(prompt("Enter a number: "));
-    var number2 = parseInt(prompt("Enter another number: "));
-    alert(add(number1, number2));
+
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(add(number1, number2));
+
+function subtract(number1, number2) {
+    return number1 - number2;
+    }
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(subtract(number1, number2));
+
+function multiply(number1, number2) {
+    return number1 * number2;
+    }
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(multiply(number1, number2));
+
+function divide(number1, number2) {
+    return number1 / number2;
+    }
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(divide(number1, number2));
